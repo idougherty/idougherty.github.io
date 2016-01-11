02645d48bfe177566ec0fcb5ad645c9d655fc2ef
idougherty <idougherty@cps.edu>
2016-01-11 14:37:18 +0000

    added dodging thing

--- brumblehum/dungeon.js
+++ brumblehum/dungeon.js
@@ -1,101 +1,100 @@
-var whileKeyDownX = null;
-var whileKeyDownY = null;
-var runGravity = null;
-var countX = 0;
-var countY = 0;
-var gravity = 0.5;
+window.onload = function() {
+  var canvas = document.getElementById("paper");
+  var c = canvas.getContext("2d");
+  var whileKeyDownX = null;
+  var runGravity = null;
+  var countX = 0;
+  var gravity = 0.5;
+	var i;
+	var willRunInto = 0;
+  
+  var player = {
+    x: 60,
+    y: 20,
+    height: 20,
+    width: 20
+  };
 
-var player = {
-  x: 60,
-  y: 10,
-  height: 20,
-  width: 20
-};
-
-var box = {
-  x: 50,
-  y: 200,
-  height: 40,
-  width: 40
-};
-
-var box2 = {
-  x: 150,
-  y: 200,
-  height: 40,
-  width: 40
-};
-
-var box3 = {
-  x: 10,
-  y: 240,
-  height: 40,
-  width: 220
-};
-
-function gravityFun() {
-	player.y += gravity;
-  $("#player").css({top: "+=" + gravity});
-  if(((player.y + player.height >= box.y) && (player.y + player.height <= box.y + box.height)) && ((player.x + player.width >= box.x) && (player.x <= box.x + box.width))) {
-  	gravity = 0;
-  	$("#player").css({top: box.y - player.height});
-  	player.y = box.y - player.height;
-  } else if(((player.y + player.height >= box2.y) && (player.y + player.height <= box2.y + box2.height)) && ((player.x + player.width >= box2.x) && (player.x <= box2.x + box2.width))) {
-    gravity = 0;
-  	$("#player").css({top: box2.y - player.height});
-  	player.y = box2.y - player.height;
-  } else if(((player.y + player.height >= box3.y) && (player.y + player.height <= box3.y + box3.height)) && ((player.x + player.width >= box3.x) && (player.x <= box3.x + box3.width))) {
-    gravity = 0;
-  	$("#player").css({top: box3.y - player.height});
-  	player.y = box3.y - player.height;
-  } else {
-  	gravity += 0.1;
-  }
-}
-
-runGravity = setInterval(gravityFun, 10);
-
-$(document).keydown(function(e) {
-	switch(e.which) {
-  	case 37:
-    	if(countX === 0) {
-      	countX = 1;
-      	whileKeyDownX = setInterval(function() {
-      	    if(((player.x - 1 <= box.x + box.width) && (player.x + player.width >= box.x)) && ((player.y + player.height - 1 >= box.y) && (player.y <= box.y + box.height)) || ((player.x - 1 <= box2.x + box2.width) && (player.x + player.width >= box2.x)) && ((player.y + player.height - 1 >= box2.y) && (player.y <= box2.y + box2.height)) || ((player.x - 1 <= box3.x + box3.width) && (player.x + player.width >= box3.x)) && ((player.y + player.height - 1 >= box3.y) && (player.y <= box3.y + box3.height))) {
-      	    } else {
-      	        $("#player").css({left: "-=1"});
-                player.x -= 1;
-      	    }
-        }, 8);
+  var collisionBoxes = [{x: 50, y: 100, height: 40, width: 40}, {x: 150, y: 100, height: 40, width: 40}, {x: 10, y: 140, height: 40, width: 220}];
+  
+  function checkCollision() {
+    for(var i = 0; i < collisionBoxes.length; i++) {
+      if(((player.y + player.height >= collisionBoxes[i].y) && (player.y + player.height <= collisionBoxes[i].y + collisionBoxes[i].height)) && ((player.x + player.width - 2 >= collisionBoxes[i].x) && (player.x + 2 <= collisionBoxes[i].x + collisionBoxes[i].width))) {
+        gravity = 0;
+    	  player.y = collisionBoxes[i].y - player.height;
+      } else {
+    	  gravity += 0.1;
       }
-      break;
-  	case 38:
-      if(gravity === 0) {
-      	gravity = -4;
-      }
-      break;
-  	case 39:
-    	if(countX === 0) {
-      	countX = 1;
-      	whileKeyDownX = setInterval(function() {
-      	    if(((player.x + player.width + 1 >= box.x) && (player.x <= box.x + box.width)) && ((player.y + player.height - 1 >= box.y) && (player.y <= box.y + box.height)) || ((player.x + player.width + 1 >= box2.x) && (player.x <= box2.x + box2.width)) && ((player.y + player.height - 1 >= box2.y) && (player.y <= box2.y + box2.height)) || ((player.x + player.width + 1 >= box3.x) && (player.x <= box3.x + box3.width)) && ((player.y + player.height - 1 >= box3.y) && (player.y <= box3.y + box3.height))) {
-      	    } else {
-      	        $("#player").css({left: "+=1"});
-                player.x += 1;
-      	    }
-        }, 8);
-      }
-      break;
-    default:
+    }
   }
-});
+  
+  function gravityFun() {
+  	player.y += gravity;
+  	checkCollision();
+  }
 
-$(document).keyup(function(e) {
-	if(e.which == 37 || e.which == 39) {
-  	clearInterval(whileKeyDownX);
-    countX = 0;
-  } else if(e.which == 38 || e.which == 40) {
-  	clearInterval(whileKeyDownY);
-    countY = 0;
-  }
-});
+  runGravity = setInterval(gravityFun, 10);
+  
+  $(document).keydown(function(e) {
+	  switch(e.which) {
+  	  case 37:
+      	if(countX === 0) {
+        	countX = 1;
+        	whileKeyDownX = setInterval(function() {
+            for(i = 0; i < collisionBoxes.length; i++) {
+              if(((player.x - 1 <= collisionBoxes[i].x + collisionBoxes[i].width) && (player.x + player.width <= collisionBoxes[i].x)) && ((player.y + player.height >= collisionBoxes[i].y) && (player.y <= collisionBoxes[i].y + collisionBoxes[i].height))) {
+                willRunInto = 1;
+              }
+            }
+            if(willRunInto !== 1) {
+              player.x -= 1;
+        	  }
+        	  willRunInto = 0;
+          }, 20);
+      	}
+        break;
+    	case 38:
+        if(gravity === 0) {
+        	gravity = -6;
+        }
+        break;
+    	case 39:
+      	if(countX === 0) {
+        	countX = 1;
+        	whileKeyDownX = setInterval(function() {
+            for(i = 0; i < collisionBoxes.length; i++) {
+              if(((player.x + player.width + 1 >= collisionBoxes[i].x) && (player.x <= collisionBoxes[i].x + collisionBoxes[i].width)) && ((player.y + player.height - 1 >= collisionBoxes[i].y) && (player.y <= collisionBoxes[i].y + collisionBoxes[i].height))) {
+        	      willRunInto = 1;
+        	    }
+            }
+            if(willRunInto !== 1) {
+              player.x += 1;
+            }
+            willRunInto = 0;
+        	}, 20);
+        }
+        break;
+      default:
+	  }
+  });
+
+  $(document).keyup(function(e) {
+	  if(e.which == 37 || e.which == 39) {
+      countX = 0;
+  	  clearInterval(whileKeyDownX);
+    }
+  });
+  
+  setInterval(function() {
+    c.fillStyle = "rgba(255, 255, 255, 0.3)";
+    c.fillRect(0, 0, canvas.width, canvas.height);
+    c.fillStyle = "#aa00aa";
+    for(i = 0; i < collisionBoxes.length; i++) {
+      c.fillRect(collisionBoxes[i].x, collisionBoxes[i].y, collisionBoxes[i].width, collisionBoxes[i].height);
+    }
+    
+    c.fillStyle = "#00aaaa";
+    c.fillRect(player.x, player.y, player.width, player.height);
+  }, 30);
+};
+//player still link: http://makepixelart.com/peoplepods/files/images/2100034.original.png
---
+++ dodge/dodging.js
@@ -0,0 +1,155 @@
+window.onload = function() {
+  var canvas = document.getElementById("paper");
+  var c = canvas.getContext("2d");
+  var keyDownX;
+  var keyDownY;
+  var keyAlreadyDownX = 0;
+  var keyAlreadyDownY = 0;
+  var gapPosTop = Math.floor((Math.random() * canvas.width * 2 / 5) + (canvas.width / 2));
+  var gapPosLeft = Math.floor((Math.random() * canvas.height * 2 / 5) + (canvas.height / 2));
+  var speed = 1;
+  var score = 0;
+  
+  var player = {
+    x: canvas.width / 2 - 10,
+    y: canvas.height / 2 - 10,
+    width: 20,
+    height: 20
+  };
+  
+  var crusher = {
+	  top: {
+		  left: {
+			  x: 0,
+			  y: 0,
+			  width: 220,
+			  height: 40
+		  },
+		  right: {
+			  x: 280,
+			  y: 0,
+			  width: 220,
+			  height: 40
+		  }
+	  },
+	  left: {
+		  top: {
+			  x: 0,
+			  y: 0,
+			  width: 40,
+			  height: 220
+		  },
+		  bottom: {
+			  x: 0,
+			  y: 280,
+			  width: 40,
+			  height: 220
+		  }
+	  }
+  };
+  
+  document.body.addEventListener('keydown', function(e) {
+    switch(e.which) {
+      case 37:
+        if((keyAlreadyDownX === 0)) {
+          keyAlreadyDownX = 1;
+          keyDownX = setInterval(function() {
+			  if(player.x >= 0) {
+				player.x -= 4;
+			  }
+          }, 30);
+        }
+        break;
+      case 38:
+        if((keyAlreadyDownY === 0)) {
+          keyAlreadyDownY = 1;
+          keyDownY = setInterval(function() {
+			  if(player.y >= 0) {
+				  player.y -= 4;  
+			  }
+          }, 30);
+        }
+        break;
+      case 39:
+        if((keyAlreadyDownX === 0)) {
+          keyAlreadyDownX = 1;
+          keyDownX = setInterval(function() {
+			  if(player.x + player.width <= canvas.width) {
+				player.x += 4;
+			  }
+          }, 30);
+        }
+        break;
+      case 40:
+        if((keyAlreadyDownY === 0)) {
+          keyAlreadyDownY = 1;
+          keyDownY = setInterval(function() {
+			  if(player.y + player.width <= canvas.height) {
+				  player.y += 4;  
+			  }
+          }, 30);
+        }
+        break;
+      default:
+    }
+  });
+  
+  document.body.addEventListener('keyup', function(e) {
+    switch(e.which) {
+      case 37:
+      case 39:
+        clearInterval(keyDownX);
+        keyAlreadyDownX = 0;
+        break;
+      case 38:
+      case 40:
+        clearInterval(keyDownY);
+        keyAlreadyDownY = 0;
+        break;
+      default:
+    }
+  });
+  
+  setInterval(function() {
+    c.fillStyle = "rgba(255, 255, 255, 0.3)";
+    c.fillRect(0, 0, canvas.width, canvas.height);
+	
+	if(crusher.top.left.y <= canvas.height) {
+		crusher.top.left.y += speed;
+		crusher.top.right.y += speed;
+		crusher.left.top.x += speed;
+		crusher.left.bottom.x += speed;
+	} else {
+		gapPosTop = Math.floor((Math.random() * canvas.width * 2 / 5) + (canvas.width / 2));
+		gapPosLeft = Math.floor((Math.random() * canvas.height * 2 / 5) + (canvas.height / 2));
+		
+		crusher.top.left.width = gapPosTop;
+		crusher.top.right.x = gapPosTop + 60;
+		crusher.top.right.width = canvas.width - (gapPosTop + 60);
+		
+		crusher.left.top.height = gapPosLeft;
+		crusher.left.bottom.y = gapPosLeft + 60;
+		crusher.left.bottom.height = canvas.width - (gapPosLeft + 60);
+		
+		crusher.top.left.y = 0;
+		crusher.top.right.y = 0;
+		crusher.left.top.x = 0;
+		crusher.left.bottom.x = 0;
+		
+		if(speed <= 4) {
+			speed += 0.5;
+		} else if(speed <= 5){
+			speed += 0.1;
+		}
+	}
+	
+	c.fillStyle = "#ff6666";
+	c.fillRect(crusher.top.left.x, crusher.top.left.y, crusher.top.left.width, crusher.top.left.height);
+	c.fillRect(crusher.top.right.x, crusher.top.right.y, crusher.top.right.width, crusher.top.right.height);
+	c.fillRect(crusher.left.top.x, crusher.left.top.y, crusher.left.top.width, crusher.left.top.height);
+	c.fillRect(crusher.left.bottom.x, crusher.left.bottom.y, crusher.left.bottom.width, crusher.left.bottom.height);
+	
+    c.fillStyle = "#00aaaa";
+    c.fillRect(player.x, player.y, player.width, player.height);
+  }, 30);
+};

1a64de6e28d961d7d5612220c66f3d6ebc4aaf1d
Ian Dougherty <idougherty@cps.edu>
2020-04-12 18:55:54 -0500

    Add files via upload

--- sqyrm/main.js
+++ sqyrm/main.js
@@ -1,9 +1,51 @@
 canvas = document.getElementById("canvas");
 c = canvas.getContext("2d");
 
+function Controller() {
+	this.displays = ["sqyrm", "route"];
+	this.transitions = ["pixelate"];
+	
+	this.displayIDX = 0;
+	this.transitionIDX = 0;
+	
+	this.display = new RouteRunner();
+	this.transition = null;
+	this.transitioning = false;
+	
+	this.pickDisplay = function() {
+		this.displayIDX = (this.displayIDX+1>=this.displays.length) ? 0:this.displayIDX+1;
+		switch(this.displays[this.displayIDX]) {
+			case "sqyrm":
+				this.display = new SqyrmRunner();
+				break;
+			case "route":
+				this.display = new RouteRunner();
+				break;
+			default:
+				throw new Error("unknown display type");
+		}
+	}
+	
+	this.pickTransition = function() {
+		this.transitionIDX = Math.floor(Math.random()*this.transitions.length);
+		switch(this.transitions[this.transitionIDX]) {
+			case "pixelate":
+				this.transition = new Pixelate();
+				break;
+			default:
+				throw new Error("unknown transition type");
+		}
+	}
+}
+
+var controller = new Controller();
+controller.display.setup();
+
 function resizeCanvas() {
 	canvas.width = window.innerWidth;
 	canvas.height = window.innerHeight;
+	
+	c.globalCompositeOperation = controller.display.blendMode;
 }
 
 window.onresize = resizeCanvas;
@@ -16,43 +58,6 @@ document.addEventListener("click", function() {
 		controller.transition.setup();
 	}
 });
-
-function Controller() {
-	this.displays = ["sqyrm"];
-	this.transitions = ["pixelate"];
-	
-	this.displayIDX = 0;
-	this.transitionIDX = 0;
-	
-	this.display = new SqyrmRunner();
-	this.transition = null;
-	this.transitioning = false;
-	
-	this.pickDisplay = function() {
-		this.displayIDX = (this.displayIDX+1>=this.displays.length) ? 0:this.displayIDX+1;
-		switch(this.displays[this.displayIDX]) {
-			case "sqyrm":
-				this.display = new SqyrmRunner();
-				break;
-			default:
-				throw new Error("unknown display type");
-		}
-	};
-	
-	this.pickTransition = function() {
-		this.transitionIDX = Math.floor(Math.random()*this.displays.length);
-		switch(this.transitions[this.transitionIDX]) {
-			case "pixelate":
-				this.transition = new Pixelate();
-				break;
-			default:
-				throw new Error("unknown transition type");
-		}
-	};
-}
-
-var controller = new Controller();
-controller.display.setup();//please be here cmon githubs
 
 setInterval(function() {	
 	c.clearRect(0, 0, canvas.width, canvas.height);
